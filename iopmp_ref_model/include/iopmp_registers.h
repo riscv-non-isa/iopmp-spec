dc62373eaf1c22f852f4abbb71302577455bdef2
gullahmed1 <gull.ahmed@10xengineers.ai>
2025-12-17 17:11:11 +0500

    Add license in file headers

--- iopmp_ref_model/include/iopmp_registers.h
+++ iopmp_ref_model/include/iopmp_registers.h
@@ -1,4 +1,8 @@
 /***************************************************************************
+// Copyright (c) 2025 by 10xEngineers.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+//
 // Authors: Mazhar Ali (mazhar.ali@10xengineers.ai)
 //          Gull Ahmed (gull.ahmed@10xengineers.ai)
 // Date: October 21, 2024
