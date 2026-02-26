1fe7683654e3d5b77fee37506cba18da92b83b66
Alvin Chang <alvinga@andestech.com>
2026-02-26 16:22:03 +0800

    libiopmp: Update the year of license header

--- libiopmp/src/iopmp_drv_common.h
+++ libiopmp/src/iopmp_drv_common.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018-2025 Andes Technology Corporation. All rights reserved.
+ * Copyright 2018-2026 Andes Technology Corporation. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
